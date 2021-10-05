69ff879a62a91b1e1e6141afd184225bfe318a95
rishav <rishavraj68@gmail.com>
2021-10-05 17:05:28 +0530

    Add base code into repo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="pokemon">
+      Hello
+    </div>
+  );
+}
+
+export default App;
